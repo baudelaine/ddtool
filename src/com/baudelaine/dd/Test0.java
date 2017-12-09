6104da5d2e5c98f387656df3bded4747f9592ebb
root <root@ODRBIMODSVR.odr1.odroa.com>
2017-12-09 21:02:51 +0100

    Test0

--- src/com/baudelaine/dd/Test0.java
+++ src/com/baudelaine/dd/Test0.java
@@ -2,6 +2,7 @@ package com.baudelaine.dd;
 
 import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 
@@ -19,20 +20,34 @@ public class Test0 {
 			AS400JDBCConnection con = (AS400JDBCConnection) DriverManager.getConnection("jdbc:as400://172.16.2.70", "IBMIIC", "spcspc");
 			AS400JDBCDatabaseMetaData metaData = (AS400JDBCDatabaseMetaData) con.getMetaData();
 			
+			Statement stmt = con.createStatement();		
+			ResultSet rst = null;
+			String query = "";
+
+			query = "SELECT constraint_name, constraint_type, table_name FROM qsys2.syscst WHERE table_schema = 'LAMFIC_V3'";
+			query = "SELECT * FROM qsys2.sysschemas";
+			//query = "SELECT * FROM qsys2.syscst WHERE table_schema = 'LAMFIC_V3'";
+			//query = "select * from LAMFIC_V3.ANA5PF FETCH FIRST 1 ROWS ONLY";
 			
-			ResultSet rst =  metaData.getExportedKeys(null, "LAMFIV_V3", "ANA5PF");
+			//rst =  metaData.getImportedKeys(null, "LAMFIC_V3", "ANA5PF");
+			rst = stmt.executeQuery(query);
+			System.out.println(query);
+			System.out.println("rst=" + rst);
 			
 			ResultSetMetaData rsmd = rst.getMetaData();
 			int colCount = rsmd.getColumnCount();
+			System.out.println("rsmd.getColumunCount()=" + colCount);
 			
+			for(int col = 1; col <= colCount; col++){
+				System.out.print(rsmd.getColumnLabel(col) + ";");
+			}
+			System.out.println("\n----------------------------------------");
 			while(rst.next()){
 				for(int col = 1; col <= colCount; col++){
-					System.out.println(rsmd.getColumnLabel(col));
-					System.out.println(rst.getString(col));
+					System.out.print(rst.getString(col) + ";");
 				}
 			}
-			
-			
+			System.out.println("\n----------------------------------------");
 			
 		} catch (ClassNotFoundException | SQLException e) {
 			// TODO Auto-generated catch block
